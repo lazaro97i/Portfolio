32fab68357eb87612b2ab82e5ee8e414f8018758
Lazaro Tomas Del Prado <lazarodelprado97@gmail.com>
2023-08-23 01:18:04 -0300

    tecnologias solucionado

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
+import projects from "./projects.js"
+
 const btnHabi = document.getElementById("btnHabilities")
 const habilities = document.getElementById("habilities")
 const top = document.getElementById("btnUp")
+const artProjects = document.getElementById("articleProjects")
+const tec = document.getElementById("tec")
 
+const renderProject = () => {
+  let fragment = ""
+  fragment += `<div class="w-[250px] bg-slate-500">
+    <img class="w-full" src="./assets/images/ataraxia.png" alt="ataraxia" />
+      <h2>${projects[0].name}</h2>
+      <p>${projects[0].category}</p>
+      <div id="tec" class="flex items-center justify-center flex-wrap gap-2">
+       ${
+        projects[0].technologies.map((t) => {
+          return `<p class="px-1 text-[10px] bg-[#1c2a4f] text-[#9cc9f4] border border-[#9cc9f4] rounded-md text-center">${t}</p>`
+        }).join("")
+       }
+      </div >
+  </div > `
+  artProjects.innerHTML = fragment
+}
 
-btnHabi.addEventListener("click", (e)=> {
+renderProject()
+
+btnHabi.addEventListener("click", (e) => {
   const href = habilities.offsetTop
-  window.scroll({top: href, behavior: "smooth"})
+  window.scroll({ top: href, behavior: "smooth" })
 })
-top.addEventListener("click", (e)=> {
+top.addEventListener("click", (e) => {
   window.scroll({
-    top:0,
+    top: 0,
     behavior: "smooth"
   })
 })
 
-window.addEventListener("scroll", (e)=> {
-  if(window.scrollY > 500){
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 500) {
     top.classList.add("w-12")
     top.classList.remove("w-0")
-  }else{
+  } else {
     top.classList.remove("w-12")
     top.classList.add("w-0")
   }
-  console.log(window.scrollY)
 })
---
+++ projects.js
@@ -0,0 +1,19 @@
+
+const projects = [
+  {
+    name: "Ataraxia Cerveceria",
+    image: "./assets/images/ataraxia.png",
+    date: "01/2023 - 03/2023",
+    technologies: ["JavaScript","React", "Redux", "React Native", "Node", "Express", "MongoDB", "JWT"],
+    category: "E-Comerce",
+    description: "Tienda digital para Cerveceria Artesanal Ataraxia",
+    repositories: {
+      back: "git.com",
+      front: "git.com",
+      mobile: "git.com"
+    }
+
+  }
+]
+
+export default projects
