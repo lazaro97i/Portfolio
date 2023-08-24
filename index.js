1831d5c6ba3b8747af5f45242dfaa4359044dfa2
Lazaro Tomas Del Prado <lazarodelprado97@gmail.com>
2023-08-23 22:12:42 -0300

    se agregan proyectos

--- index.js
+++ index.js
@@ -4,22 +4,35 @@ const btnHabi = document.getElementById("btnHabilities")
 const habilities = document.getElementById("habilities")
 const top = document.getElementById("btnUp")
 const artProjects = document.getElementById("articleProjects")
-const tec = document.getElementById("tec")
 
 const renderProject = () => {
   let fragment = ""
-  fragment += `<div class="w-[250px] bg-slate-500">
-    <img class="w-full" src="./assets/images/ataraxia.png" alt="ataraxia" />
-      <h2>${projects[0].name}</h2>
-      <p>${projects[0].category}</p>
-      <div id="tec" class="flex items-center justify-center flex-wrap gap-2">
+  fragment += projects.map((p) => {
+    return `<div class="w-[250px] bg-[#101628] rounded-b-md">
+      <div class="relative z-0">
+      <a href="${p.deploy}" target="_blank" class="w-full absolute bg-black z-0 h-[123px] bg-opacity-30 md:hover:bg-opacity-0 cursor-pointer transition-all duration-300"></a>
+      <img class="w-full h-[123px] object-contain bg-white z-0" src="${p.image}" alt="ataraxia" />
+      </div>
+      <h2 class="text-xl w-full text-start pl-3 mt-2 text-[#9cc9f4]">${p.name}</h2>
+      <p class="text-sm pl-3 mb-2">${p.category}</p>
+      <div class="contact-head flex px-3 py-2 mb-2 items-center justify-evenly flex-wrap gap-1">
        ${
-        projects[0].technologies.map((t) => {
-          return `<p class="px-1 text-[10px] bg-[#1c2a4f] text-[#9cc9f4] border border-[#9cc9f4] rounded-md text-center">${t}</p>`
+        p.technologies.map((t) => {
+          return `<p key="${t}" class="px-1 text-[10px] bg-[#1c2a4f] text-[#9cc9f4] border border-[#9cc9f4] rounded-md text-center">${t}</p>`
         }).join("")
        }
       </div >
-  </div > `
+      <div class="w-full flex flex-wrap">
+      <p class="pl-3 pb-2">Respositorios:</p>
+       <div class="flex justify-around px-3 border-b border-[#2a4ba3] w-full pb-3">
+        <a class="font-[500] md:hover:text-[#9cc9f4] transition-all duration-200" target="_blank" href="${p.repositories.front}" >Front</a>
+        <a class="font-[500] md:hover:text-[#9cc9f4] transition-all duration-200" target="_blank" href="${p.repositories.back}" >Back</a>
+        <a class="font-[500] ${p.repositories.mobile ? null : "pointer-events-none"} md:hover:text-[#9cc9f4] transition-all duration-200" target="_blank" href="${p.repositories.mobile}" >${p.repositories.mobile ? "Mobile" : "-"}</a>
+       </div>
+       <a class="${p.deploy ? null : "pointer-events-none"} btnDeploy font-[500] w-full text-center py-1 bg-[#1e2f5c] rounded-b-md hover:bg-[#1e356e] transition-all duration-300" href="${p.deploy}" target="_blank">${p.deploy ? "Deploy" : "-"}</a>
+      </div>
+    </div > `
+  }).join("")
   artProjects.innerHTML = fragment
 }
 
--- projects.js
+++ projects.js
@@ -8,11 +8,39 @@ const projects = [
     category: "E-Comerce",
     description: "Tienda digital para Cerveceria Artesanal Ataraxia",
     repositories: {
-      back: "git.com",
-      front: "git.com",
-      mobile: "git.com"
-    }
-
+      back: "https://github.com/feldrok/ataraxia-back",
+      front: "https://github.com/feldrok/ataraxia-front",
+      mobile: "https://github.com/feldrok/ataraxia-mobile"
+    },
+    deploy: "https://ataraxia-front.onrender.com/"
+  },
+  {
+    name: "Minga",
+    image: "./assets/images/gitProject.png",
+    date: "12/2022 - 02/2023",
+    technologies: ["JavaScript","React", "Redux", "React Native", "Node", "Express", "MongoDB", "JWT"],
+    category: "Lectura de mangas",
+    description: "Lectura de mangas online.",
+    repositories: {
+      back: "https://github.com/rodrigohernans/minga-backend",
+      front: "https://github.com/feldrok/minga-frontend",
+      mobile: "https://github.com/feldrok/mobile-minga-red"
+    },
+    deploy: null
+  },
+  {
+    name: "Prestamos Personales",
+    image: "./assets/images/prestamos.png",
+    date: "06/2023 - Actualidad",
+    technologies: ["JavaScript", "Vite", "React", "Redux", "Node", "Express", "MongoDB"],
+    category: "Gestion de clientes",
+    description: "Sistema de gestion de clientes para prestamos personales.",
+    repositories: {
+      back: "https://github.com/lazaro97i/prestamos",
+      front: "https://github.com/lazaro97i/prestamos",
+      mobile: null
+    },
+    deploy: "https://prestamos.onrender.com/"
   }
 ]
 
