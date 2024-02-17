2a7ac3a1cd37824d479e28cdef2f2f01bf63f353
Lazaro Tomas Del Prado <lazarodelprado97@gmail.com>
2024-02-17 02:49:36 -0300

    agrega estilos a titulo de tecnologias nuevas

--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -32,9 +32,9 @@ const projects = [
     name: "CRUD Usuarios",
     image: "./assets/images/crud_users.png",
     date: "01/2024 - 02/2024",
-    technologies: ["Java", "Spring Boot", "JavaScript", "Vite", "React", "MySQL"],
+    technologies: ["Java", "Spring-Boot", "JavaScript", "React", "MySQL"],
     category: "ABM",
-    description: "Sistema de gestión de usuarios, un proyecto diseñado con tecnologías avanzadas para ofrecer una solución robusta y eficiente. Desarrollado con Spring Boot en el backend para construir un backend eficiente que gestiona la lógica de negocio y facilita las operaciones CRUD de manera rápida y segura, Hibernate y JPA para la persistencia en MySQL, y React en el frontend, nuestro sistema permite realizar operaciones esenciales de Crear, Leer, Actualizar y Eliminar (CRUD) usuarios de manera intuitiva y ágil.",
+    description: "Sistema de gestión de usuarios, un proyecto diseñado con tecnologías avanzadas para ofrecer una solución robusta y eficiente. Desarrollado con Spring Boot para construir un backend eficiente que gestiona la lógica de negocio y facilita las operaciones CRUD de manera rápida y segura, Hibernate y JPA para la persistencia en MySQL, y React en el frontend, el sistema permite realizar operaciones esenciales de Crear, Leer, Actualizar y Eliminar usuarios de manera intuitiva y ágil.",
     repositories: {
       back: "https://github.com/lazaro97i/abm_user_back",
       front: "https://github.com/lazaro97i/abm_usuarios/tree/main/front",
